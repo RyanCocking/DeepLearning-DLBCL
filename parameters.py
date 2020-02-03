5453950fd52c2cc47308a5970f9fda2936d1d479
RyanCocking <bsrctb@leeds.ac.uk>
2020-02-03 20:18:31 +0000

    Fixed directory in plot.py

--- parameters.py
+++ parameters.py
@@ -1,7 +1,7 @@
 dir_wsl_to_edrive = "/mnt/e/OneDrive/Documents/UNIVERSITY/PhD/WellcomeTrust/rotation2"    # for use only when using WSL on my Windows 10 home PC
 dir_linux = ""    # main code directory
 dir_main = dir_wsl_to_edrive
-dir_figs = "{0}/figures".format(dir_main)    # experimental figures
+dir_figures = "{0}/figures".format(dir_main)    # experimental figures
 dir_slides = "{0}/slides".format(dir_main)    # pathology slide images
 dir_slide_data = "{0}/slide_data".format(dir_main)    # further info on the pathology slides
 
--- plot.py
+++ plot.py
@@ -2,4 +2,4 @@ import matplotlib.pyplot as plt
 import parameters as param
 
 plt.plot([1,2,3],[1,2,3],'ro')
-plt.savefig("{0}/test.png".format(param.wsl_edrive), dpi=400)
+plt.savefig("{0}/test.png".format(param.dir_figures), dpi=400)

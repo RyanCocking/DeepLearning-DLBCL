c5dba4696c8863e7a0751ea2839f1aa3dd1796fc
RyanCocking <bsrctb@leeds.ac.uk>
2020-02-03 20:16:48 +0000

    Changed WSL folder location

--- parameters.py
+++ parameters.py
@@ -1,2 +1,7 @@
-dir_wsl_edrive = "/mnt/e/Code/WTRotation2_FILES"  # for use only when using WSL on my Windows 10 home PC
-dir_test_figs = "{0}/test/".format(dir_wsl_edrive)  # experimental figures
+dir_wsl_to_edrive = "/mnt/e/OneDrive/Documents/UNIVERSITY/PhD/WellcomeTrust/rotation2"    # for use only when using WSL on my Windows 10 home PC
+dir_linux = ""    # main code directory
+dir_main = dir_wsl_to_edrive
+dir_figs = "{0}/figures".format(dir_main)    # experimental figures
+dir_slides = "{0}/slides".format(dir_main)    # pathology slide images
+dir_slide_data = "{0}/slide_data".format(dir_main)    # further info on the pathology slides
+

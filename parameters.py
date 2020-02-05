1245c9062b4e5b01be4e8d2b9c4d834786846475
RyanCocking <bsrctb@leeds.ac.uk>
2020-02-05 19:13:53 +0000

    Adjusted folder names

--- parameters.py
+++ parameters.py
@@ -1,7 +1,8 @@
 dir_wsl_to_edrive = "/mnt/e/OneDrive/Documents/UNIVERSITY/PhD/WellcomeTrust/rotation2"    # for use only when using WSL on my Windows 10 home PC
 dir_linux = ""    # main code directory
 dir_main = dir_wsl_to_edrive
-dir_figures = "{0}/figures".format(dir_main)    # experimental figures
-dir_slides = "{0}/slides".format(dir_main)    # pathology slide images
-dir_slide_data = "{0}/slide_data".format(dir_main)    # further info on the pathology slides
+dir_figures = "{0}/figures".format(dir_main)
+dir_slides_raw = "{0}/slides_raw".format(dir_main)    # pathology slides (.svs)
+dir_slides_cropped = "{0}/slides_cropped".format(dir_main)    # processed slide images (.png)
+dir_slide_data = "{0}/slide_data".format(dir_main)    # slide info spreadsheets
 

559f5d0fb2144fa2cc41c96ad9e2d4970ee3c6fd
RyanCocking <bsrctb@leeds.ac.uk>
2020-02-14 12:07:14 +0000

    Writing method to extract alphanumeric core references from spreadsheet

--- load_slide.py
+++ load_slide.py
@@ -3,27 +3,56 @@ import openslide as opsl
 import parameters as parm
 import pandas as pd
 
-def get_slide_ids(filename):
+def get_spreadsheet_info(filename):
     """
-    Load Excel spreadsheet and extract slide IDs for BCL-2 and c-MYC
+    Load Excel spreadsheet and extract info such as slide IDs for BCL-2
+    and c-MYC, number of columns and rows, pathology data, etc.
+
+    A 'slide' refers to the collection of circular tissue sample 'cores' 
+    contained within a single .svs file. 
+    
+    A single spreadsheet contains several sheets, each containing info on 
+    one or more slides.
+
+    arguments:
+        filename: .svs file path
+
+    returns:
+        BCL2_slide_id: N int array, 7-digit slide IDs for BCL-2 staining
+        cMYC_slide_id: N int array, 7-digit slide IDs for c-MYC staining
+        core_refs: NxMx2 int array, alphanumeric core references for N slides
+                   of M cores. Indexed per core as [ABC, 123] i.e. [col, row]
     """
-    # Load pathology data from .xlsx file as a dict of sheets
+    # Load .xlsx file as a dict of sheets
     print("Loading slide data from spreadsheet...")
     df = pd.read_excel(filename, None)    # pandas DataFrame
 
     BCL2_slide_id=[]
     cMYC_slide_id=[]
+    core_refs = []
+    tumour = []
+    host = []
+    stroma = []
 
+    # sheets within a spreadsheet
     for sheetname in df.keys():
         sheet = df[sheetname]    # can be printed to display table contents
 
-        col_d = sheet.columns[3]    # columns D and H
+        col_d = sheet.columns[3]    # spreadsheet columns D and H
         col_h = sheet.columns[7]
 
         BCL2_slide_id.append(sheet[col_d][2])    # slide ID (raw image names)
         cMYC_slide_id.append(sheet[col_h][2])
 
-    return BCL2_slide_id, cMYC_slide_id
+        col_b = sheet.columns[1] 
+        col_c = sheet.columns[2]
+
+        print(sheet[col_b][4])
+        print(sheet[col_c][4])
+
+        #for cores in 
+
+    return BCL2_slide_id, cMYC_slide_id, core_refs
 
 def print_slide_metadata(slide_object):
     """
--- main.py
+++ main.py
@@ -3,14 +3,18 @@ import cv2
 import numpy as np
 
 import parameters as parm
-from load_slide import get_slide_ids, print_slide_metadata
+from load_slide import get_spreadsheet_info, print_slide_metadata
 from edge_detection import detect_circles
 
 # Slide IDs of ABC gene
 print("Obtaining slide IDs of ABC data...")
-abc_BCL2_slide_id, abc_cMYC_slide_id = get_slide_ids("{0}/ALL_REMoDL-B_TMA_ABC_RT.xlsx".format(parm.dir_slide_data))
+abc_BCL2_slide_id, abc_cMYC_slide_id, abc_core_refs = get_spreadsheet_info(
+    "{0}/ALL_REMoDL-B_TMA_ABC_RT.xlsx".format(parm.dir_slide_data))
+
 print("BCL-2 slide IDs: ", abc_BCL2_slide_id)
 print("c-MYC slide IDs: ", abc_cMYC_slide_id)
+
+quit()
 
 # BCL-2
 for slide_id in abc_BCL2_slide_id[0:2]:
@@ -18,9 +22,9 @@ for slide_id in abc_BCL2_slide_id[0:2]:
     print("Slide ID = {0}".format(slide_id))
     print("==================")
 
-    # Open slide object, read region, save as PNG
+    # Pointer to slide object
     print("Opening slide object...")
-    my_slide = opsl.OpenSlide("{0}/{1}.svs".format(parm.dir_slides_raw, slide_id))  # pointer to slide object
+    my_slide = opsl.OpenSlide("{0}/{1}.svs".format(parm.dir_slides_raw, slide_id))
     # print_slide_metadata(my_slide)
 
     # xdim = slide_metadata.level[zoom].width
@@ -31,19 +35,30 @@ for slide_id in abc_BCL2_slide_id[0:2]:
     ycorner = 0
     xdim = 2000
     ydim = 2000
+    # (x,y) of top-left corner, zoom level (0 = max zoom), (w,h) pixels
     slide_image = my_slide.read_region(location=(xcorner, ycorner), level=zoom, 
-        size=(xdim, ydim))    # (x,y) of top-left corner, zoom level (0 = max zoom), (w,h) pixels
+        size=(xdim, ydim))    
 
-    slide_image.save("{0}/slide_test.png".format(parm.dir_figures))  # save slide object as png
+    slide_image.save("{0}/slide_test.png".format(parm.dir_figures))
     print("Saved PNG to {0}/{1}_slide_test.png".format(parm.dir_figures, slide_id))
 
     # Find circular cores within an image 
     print("Detecting circles in image...")
-    centres, radii = circle_img = detect_circles(in_file="{0}/slide_test.png".format(parm.dir_figures),
-        out_file="{0}/{1}_circles.png".format(parm.dir_figures, slide_id), p1=50, p2=30, minr=50, maxr=80)
+    centres, radii = detect_circles(
+        in_file="{0}/slide_test.png".format(parm.dir_figures),
+        out_file="{0}/{1}_circles.png".format(parm.dir_figures, slide_id),
+        p1=50, p2=30, minr=50, maxr=80)
 
-    print(centres)
-    print(radii)
+    # Sort centres in-place by y pixel (found on StackOverflow)
+    centres.view('uint16,uint16').sort(order=['f1'], axis=0)
+
+    # Generate a rectangular grid based on detected core centres, to account
+    # for undetected cores
+    print_slide_metadata(my_slide)
+    xcores = my_slide.properties['openslide.level[3].width'] 
+    # ycores = 
+
+    print(xcores)
 
     print("Closing slide object...")
     my_slide.close()

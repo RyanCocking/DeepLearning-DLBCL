ff7cca5a818ff45d88484806df725e2b682ef9bb
RyanCocking <bsrctb@leeds.ac.uk>
2020-02-26 19:06:59 +0000

    Less reundant printing

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import numpy as np
 import parameters as parm
 from load_slide import get_spreadsheet_info, print_slide_metadata
 from edge_detection import detect_circles
+from crop_slides import create_grid
 
 # Slide IDs of ABC gene
 print("Obtaining slide IDs of ABC data...")
@@ -12,17 +13,13 @@ abc_BCL2_slide_id, abc_cMYC_slide_id, abc_sample_refs = get_spreadsheet_info(
     "{0}/ALL_REMoDL-B_TMA_ABC_RT.xlsx".format(parm.dir_slide_data))
 
 print("BCL-2 slide IDs:   ", abc_BCL2_slide_id[:])
-print("c-MYC slide IDs:   ", abc_cMYC_slide_id[:])
-print("Sample references for slide {0}:  {1}".format(abc_BCL2_slide_id[0], abc_sample_refs[0]))
 print("")
 
 # Loop over two slides of BCL-2
-for slide_id in abc_BCL2_slide_id[-2:]:
-
-    print("Slide ID = {0}".format(slide_id))
+for slide_id in abc_BCL2_slide_id[:2]:
 
     # Pointer to slide object
-    print("Opening slide object...")
+    print("Opening slide {0} object...".format(slide_id))
     my_slide = opsl.OpenSlide("{0}/{1}.svs".format(parm.dir_slides_raw, slide_id))
 
     print("Reading slide region...")
@@ -50,7 +47,9 @@ for slide_id in abc_BCL2_slide_id[-2:]:
     # Generate a rectangular grid based on detected sample centres, to account
     # for undetected samples
 
-    print("Closing slide object...")
+    create_grid(centres, 75, 7, 7)
+
+    print("Closing slide {0} object...".format(slide_id))
     print("")
     my_slide.close()
 

f1308646eecdf2016b2196df20626cb9dbb138c8
RyanCocking <bsrctb@leeds.ac.uk>
2020-02-27 19:42:55 +0000

    Over-counted circles are now deleted

--- edge_detection.py
+++ edge_detection.py
@@ -15,42 +15,44 @@ def detect_edges(in_file, out_file="edges.png", p1=50, p2=200):
 
 def remove_duplicate_circles(centres, radii):
     """
-    Fix double-counted sample centres.
+    Fix over-counted sample centres by comparing distances from sample i
+    to its neighbour j. Neighbours that are within a distance of 2*radius
+    are removed.
 
     arguments:
-        centres: Nx2 integer array, coordinates of circle centres detected with
+        centres: Nx2 integer numpy array, coordinates of circle centres detected with
                  the Hough transform
-        radii: N integer array, corresponding radii
+        radii: N integer numpy array, corresponding radii
 
     returns:
-        centres array, with double-counts removed
+        centres array, with extra counts removed
         corresponding radii
     """
 
     indices = []
-    for i in range(centres.shape[0]):
-        for j in range(centres.shape[0]):
-            if i != j:
-
-                print(i, j)
-
-                rad1 = radii[i]
-                rad2 = radii[j]
+    # Loop over centre pairs
+    for i in range(0, centres.shape[0] + 1):
+        for j in range(i+1, centres.shape[0]):
                 d = np.linalg.norm(centres[j] - centres[i])
 
-                # If the centres are too close, mark for deletion whichever
-                # has the smaller corresponding radius
-                if d < 2*rad1:
-                    # Current
-                    if rad2 <= rad1:
+                # overlap
+                if d < 2*radii[i]:
+                    # mark for deletion the circle with the
+                    # smallest radius
+                    if radii[i] < radii[j]:
                         indices.append(i)
-                    # Neighbour
                     else:
                         indices.append(j)
 
-        # TODO: Still needs some work
+    # Account for 3 or more circles near the same sample (NOTE: quick fix)
+    indices = np.unique(indices)
 
-        print(indices)
+    # Delete marked elements
+    centres = np.delete(centres, indices, axis=0)
+    radii = np.delete(radii, indices)
+
+    return centres, radii
+
 
 def detect_circles(in_file, out_file="circles.png", p1=50, p2=30, minr=50, maxr=80):
     """
@@ -75,27 +77,27 @@ def detect_circles(in_file, out_file="circles.png", p1=50, p2=30, minr=50, maxr=
     circles = cv2.HoughCircles(img, cv2.HOUGH_GRADIENT, 1, 20,
         param1=p1, param2=p2, minRadius=minr, maxRadius=maxr)
 
-    # Will throw complaint here if no circles detected.
+    # NOTE: Will crash and return error here if no circles detected.
 
-    # draw circles on the original unblurred image
     circles = np.array(np.around(circles), dtype='int16')
     centres = []
     radii = []
     for i in circles[0,:]:
-        # draw the outer circle
-        cv2.circle(cimg,(i[0],i[1]),i[2],(0,255,0),2)
-        # draw the center of the circle
-        cv2.circle(cimg,(i[0],i[1]),2,(0,0,255),3)
-        
         centres.append([i[0],i[1]])
         radii.append(i[2])
-
-    cv2.imwrite(out_file, cimg)
-    print("Saved PNG to {0}".format(out_file))
 
     centres = np.array(centres)
     radii = np.array(radii)
 
-    remove_duplicate_circles(centres, radii)
+    centres, radii = remove_duplicate_circles(centres, radii)
+
+    for i, c in enumerate(centres):
+        # draw the outer circle (on unblurred image)
+        cv2.circle(cimg,(c[0],c[1]),radii[i],(0,255,0),4)
+        # draw the center of the circle
+        cv2.circle(cimg,(c[0],c[1]),2,(0,0,255),4)
+
+    cv2.imwrite(out_file, cimg)
+    print("Saved PNG to {0}".format(out_file))
 
     return centres, radii

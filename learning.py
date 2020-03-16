0658bb605109176bfef2b6ec7d41d91494f16179
RyanCocking <bsrctb@leeds.ac.uk>
2020-03-16 22:00:47 +0000

    First successful compilation and training of a model! Used MobileNetV2 for now due to 2mil params vs. VGG19 20mil. Evaluation on validation data for 20 steps gives poor accuracy, as expected.

--- learning.py
+++ learning.py
@@ -6,6 +6,7 @@ import numpy as np
 import dl_parameters as parm
 from dl_routines import sort_images
 
+
 path_obj = pathlib.Path(parm.dir_img_data)
 class_names = ["ABC", "GCB"]
 
@@ -23,27 +24,83 @@ if os.path.exists(parm.dir_train) and os.path.isdir(parm.dir_train) and os.listd
     print("Training directory is not empty. Will not move image files.")
 else:
     print("Moving ABC images to training, testing and validation folders...")
-    sort_images(abc_paths, "ABC", parm.dir_train, parm.dir_test, parm.dir_val, parm.test_split, parm.val_split)
+    sort_images(abc_paths, "ABC", parm.dir_train, parm.dir_test, parm.dir_val, 
+                parm.test_split, parm.val_split)
     print("Moving GCB images to training, testing and validation folders...")
-    sort_images(gcb_paths, "GCB", parm.dir_train, parm.dir_test, parm.dir_val, parm.test_split, parm.val_split)
+    sort_images(gcb_paths, "GCB", parm.dir_train, parm.dir_test, parm.dir_val, 
+                parm.test_split, parm.val_split)
     print("Done") 
 
 
-# The 1./255 is to convert from uint8 to float32 in range [0,1].
+# The 1./255 is to convert from uint8 to float32 in range [0,1]. Can apply
+# data augmentation here.
 generator = tf.keras.preprocessing.image.ImageDataGenerator(rescale=1.0/255)
 
 # Generate batches of tensor image data, for all images
-train_gen = generator.flow_from_directory(directory=parm.dir_train,
-    batch_size=parm.batch_size, shuffle=True, 
-    target_size=(parm.img_dim, parm.img_dim), classes=list(class_names))
+print("Loading data as shuffled batches...")
+train_gen = generator.flow_from_directory(directory=parm.dir_train, 
+                                          batch_size=parm.batch_size, 
+                                          shuffle=True, 
+                                          target_size=(parm.img_dim, parm.img_dim), 
+                                          classes=list(class_names))
 
-test_gen = generator.flow_from_directory(directory=parm.dir_test,
-    batch_size=parm.batch_size, shuffle=True, 
-    target_size=(parm.img_dim, parm.img_dim), classes=list(class_names))
+test_gen = generator.flow_from_directory(directory=parm.dir_test, 
+                                         batch_size=parm.batch_size, 
+                                         shuffle=True, 
+                                         target_size=(parm.img_dim, parm.img_dim), 
+                                         classes=list(class_names))
 
-val_gen = generator.flow_from_directory(directory=parm.dir_val,
-    batch_size=parm.batch_size, shuffle=True, 
-    target_size=(parm.img_dim, parm.img_dim), classes=list(class_names))
+val_gen = generator.flow_from_directory(directory=parm.dir_val, 
+                                        batch_size=parm.batch_size, 
+                                        shuffle=True, 
+                                        target_size=(parm.img_dim, parm.img_dim), 
+                                        classes=list(class_names))
+
+img_shape = (parm.img_dim, parm.img_dim, 3)
+
+# Load the pre-trained model without the topmost classification (or 'bottleneck)
+# layers (include_top=False)
+print("Loading pre-trained model...")
+#pre_model = tf.keras.applications.vgg19.VGG19(input_shape=img_shape, 
+#                                              include_top=False, 
+#                                              weights='imagenet')
+pre_model = tf.keras.applications.MobileNetV2(input_shape=img_shape, 
+                                              include_top=False, 
+                                              weights='imagenet')
+pre_model.summary()
+
+# Freeze the convolutional base of the model prior to compilation and training.
+# This prevents weights from being updated during training.
+pre_model.trainable = False
+
+# Add a classifier to the top of the model. The pooling layer converts features
+# to a 1280-element vector per image, whilst the dense layer converts these
+# features into a single prediction per image.
+global_average_layer = tf.keras.layers.GlobalAveragePooling2D()
+prediction_layer = tf.keras.layers.Dense(1)
+
+# Stack these layers on top of the VGG model using a Keras sequential model
+print("Stacking classification layers on top of pre-trained model...")
+model = tf.keras.Sequential(layers=[pre_model, global_average_layer, prediction_layer])
+    
+# Compile the model. Use a binary cross-entropy loss function, since there are
+# only two classes
+print("Compiling combined Sequential model...")
+model.compile(optimizer=tf.keras.optimizers.RMSprop(lr=parm.learning_rate), 
+              loss=tf.keras.losses.BinaryCrossentropy(from_logits=True), 
+              metrics=['accuracy'])
+model.summary()
+
+# Run model on validation data in test mode
+print("Running model in evaluation mode...")
+loss, accuracy = model.evaluate(x=val_gen, steps=20)
+
+print("Loss = {0}".format(loss))
+print("Accuracy = {0}".format(accuracy))
+
+print("Done")
+
+
 
 
     

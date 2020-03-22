1e3a3b2d6f455fc583c3242885c346a2631dfe27
RyanCocking <bsrctb@leeds.ac.uk>
2020-03-22 17:55:18 +0000

    Count total number of samples (including NA) in a slide

--- load_slide.py
+++ load_slide.py
@@ -9,7 +9,7 @@ def get_spreadsheet_info(filename):
     Load Excel spreadsheet and extract info such as slide IDs for BCL-2
     and c-MYC, number of columns and rows, pathology data, etc.
 
-    A 'slide' refers to the collection of circular tissue sample 'cores' 
+    A 'slide' refers to the collection of circular tissue sample 'samples' 
     contained within a single .svs file. 
     
     A single spreadsheet contains several sheets, each containing info on 
@@ -21,9 +21,9 @@ def get_spreadsheet_info(filename):
     returns:
         BCL2_slide_id: N int array, 7-digit slide IDs for BCL-2 staining
         cMYC_slide_id: N int array, 7-digit slide IDs for c-MYC staining
-        core_refs: NxMx2 list, alphanumeric core references for N slides
-                   of M cores. Both stains use the same references. Indexed as
-                   [slide, core, col/row]
+        sample_refs: NxMx2 list, alphanumeric sample references for N slides
+                   of M samples. Both stains use the same references. Indexed as
+                   [slide, sample, col/row]
 
     """
     # Load .xlsx file as a dict of sheets
@@ -32,7 +32,7 @@ def get_spreadsheet_info(filename):
 
     BCL2_slide_id=[]
     cMYC_slide_id=[]
-    core_refs = []
+    sample_refs = []
 
     # sheets within a spreadsheet
     for sheetname in df.keys():
@@ -47,12 +47,19 @@ def get_spreadsheet_info(filename):
         col_b = sheet.columns[1] 
         col_c = sheet.columns[2]
 
-        core_cols = np.array(sheet[col_c][4:])
-        core_rows = np.array(sheet[col_b][4:])
+        sample_cols = np.array(sheet[col_c][4:])
+        sample_rows = np.array(sheet[col_b][4:])
 
-        core_refs.append(np.transpose([core_cols, core_rows]))
-        
-    return BCL2_slide_id, cMYC_slide_id, core_refs
+        # TODO: if sample contains no data or NA, don't append
+        sample_refs.append(np.transpose([sample_cols, sample_rows]))
+      
+        cols, counts = np.unique(sample_cols, return_counts=True)
+        num_samples = np.sum(counts)    # Includes N/A
+        print(num_samples)
+
+    return BCL2_slide_id, cMYC_slide_id, sample_refs
+
+
 
 def print_slide_metadata(slide_object):
     """

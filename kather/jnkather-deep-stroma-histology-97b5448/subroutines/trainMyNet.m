f3f849b256a88e67cf36374fdd614d06b6cd95a8
RyanCocking <bsrctb@leeds.ac.uk>
2020-04-02 14:24:19 +0100

    Deleted old files

--- kather/jnkather-deep-stroma-histology-97b5448/subroutines/trainMyNet.m
+++
@@ -1,7 +0,0 @@
-function myNet = trainMyNet(net,imds,workhorse,NumEpochs,numClasses)
-
-opts = trainingOptions('sgdm', 'InitialLearnRate', 3E-4, ... 
-    'MaxEpochs', NumEpochs, 'MiniBatchSize', 360,'ExecutionEnvironment',workhorse);
-myNet = trainNetwork(imds, layers, opts);
-
-end
